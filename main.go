c212791a38fc28f4a04ca7d55475514c2354ec73
LowkeyCoding <loke0046@gmail.com>
2020-12-22 00:08:22 +0100

    Clean up

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"./blog"
@@ -9,21 +8,9 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func index(ctx *fasthttp.RequestCtx) {
-	ctx.SendFile("./index.html")
-}
-
-func styles(ctx *fasthttp.RequestCtx) {
-	path := fmt.Sprintf("./styles/%s", ctx.UserValue("stylesheet"))
-	ctx.SendFile(path)
-}
-
 func main() {
 	router := router.New()
 	blog := blog.Blog{}
-
-	router.GET("/", index)
-	router.GET("/styles/{stylesheet}", styles)
 
 	blog.Path = "/blog/"
 	blog.Name = "LowkeyCoding's Blog"
@@ -35,7 +22,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = blog.CreateUser("LowkeyCoding", "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86")
 	if err != nil {
 		log.Println(err)
 	}
